b0b2991fad5b89050589e4c616d454adbacf6a5a
Jeeaaasus <15081909+Jeeaaasus@users.noreply.github.com>
2020-02-28 00:07:49 +0100

    Update egsfreegamesbot.py

--- egsfreegamesbot.py
+++ egsfreegamesbot.py
@@ -93,7 +93,7 @@ class bot:
             # click button
             WebDriverWait(self.driver, 60).until(available((By.XPATH, '//*[text()="Free Now"]')))
             self.driver.find_elements_by_xpath("//*[text()='Free Now']")[n].click()
-            sleep(10)
+           # sleep(10)
             print(f'#{1 + n}: {self.driver.current_url}')
             while True:
                 try:
