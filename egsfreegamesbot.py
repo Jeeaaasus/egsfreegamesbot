0d4853dc4a14fff1d71f1e7fb9123bf49b595cdd
Jeeaaasus <15081909+Jeeaaasus@users.noreply.github.com>
2020-06-12 16:55:17 +0200

    Update egsfreegamesbot.py

--- egsfreegamesbot.py
+++ egsfreegamesbot.py
@@ -61,6 +61,7 @@ class bot:
         if egs_debug: sleep(10); print(f'DEBUG:login @ {self.driver.current_url}')
         # Login with Epic account
         WebDriverWait(self.driver, 300).until(available((By.XPATH, '//*[text()="Sign in with Epic Games"]')))
+        if egs_debug: sleep(10); print(f'DEBUG:screenshot login1 @ {self.driver.current_url}'); self.driver.save_screenshot("debug_login1.png")
         # write email
         WebDriverWait(self.driver, 300).until(available((By.XPATH, '//*[@id="email"]'))).send_keys(egs_username)
         # write password
